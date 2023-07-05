5e54d206db468554fa7b313b6f762b0d9c2e923a
Blake Bishop <b.bishop3709@student.nu.edu>
2023-07-05 14:57:33 -0700

    Local: refactored create view UI

--- Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92f844c9ba888cf999db87f5485b773ecc72368f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f2555296d636f077618589f324181425e72dc17")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
