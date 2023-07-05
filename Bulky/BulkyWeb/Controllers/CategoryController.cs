a283c7f8b07c2f5724f325b92b2d9103a3571132
Blake Bishop <b.bishop3709@student.nu.edu>
2023-07-05 16:20:29 -0700

    Local: Adding Edit functions

--- Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -44,5 +44,46 @@ namespace BulkyWeb.Controllers
             return View();
             
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0) { return NotFound(); }
+
+            // made this field nullable in order to remove warnings since we have logic checks
+            // Find() only works with Primary Key
+            Category? categoryFromDb = _db.Categories.Find(id);
+
+            // other options for record retrieval
+            // Category? categoryFromDb2 = _db.Categories.FirstOrDefault(u => u.Id == id); *this is a very good option
+            // Category? categoryFromDb3 = _db.Categories.Where(u => u.Id == id).FirstOrDefault(); *this option is usually used when multiple attributes need to be considered in the Where()
+
+
+
+            if (categoryFromDb == null) { return NotFound(); }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Cateogry Name cannot match the Display Name");
+            }
+            if (obj.Name != null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("name", "Test is an invalid value");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
     }
 }
