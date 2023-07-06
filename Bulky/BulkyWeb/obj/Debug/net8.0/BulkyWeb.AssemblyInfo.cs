0f91800ae8ed5725a1489e2142386082924d6bf9
Blake Bishop <b.bishop3709@student.nu.edu>
2023-07-06 15:02:36 -0700

    Local: Added Delete function

--- Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39e4cf7a1793aaba27199edd7a06bdff81485d7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c317ec08cf912ac302f5912bd82a03ff78dbf904")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
